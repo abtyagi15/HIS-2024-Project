e9936c03761eace442cb002678430f88b975bc04
abtyagi15 <abtyagi150702@gmail.com>
2024-03-30 04:10:11 +0530

    register, login route

--- server/routes/User.js
+++ server/routes/User.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { register,login } = require('../controllers/Auth');
 
-router.post('/register',signup);
+router.post('/register',register);
 router.post('/login',login);
 
 module.exports = router;
