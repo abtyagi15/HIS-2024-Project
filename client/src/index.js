8f567375008c464e587a8cf318c03c6945989c5b
abtyagi15 <abtyagi150702@gmail.com>
2024-03-31 02:47:55 +0530

    cmmit

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+        <Toaster />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
