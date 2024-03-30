ba2cb57cc06cae9e30e3ab04e27e36526d43c2e7
Kareen Ujjainiya <kareenujjainiya222@gmail.com>
2024-03-30 17:07:05 +0530

    new updates

--- client/src/components/Tests.jsx
+++ client/src/components/Tests.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import data from "../data";
 export default function Tests(props) {
   const loadcontent = props.load;
   return (
@@ -9,39 +10,39 @@ export default function Tests(props) {
         id="test-container"
         className="container mt-5 p-3  rounded-t-3xl flex flex-wrap flex-row"
       >
-        <div className="max-w-sm m-2 bg-white hover:scale-105 rounded-lg shadow-md  bg-gradient-to-br from-green-100 to-blue-900">
+        <div className="bg-gradient-to-r mt-14 from-teal-300 via-green-100 to-white max-w-sm m-2 bg-white hover:scale-105  rounded-lg shadow-md">
           <Link onClick={loadcontent} to="/about">
             <img
-              className="rounded-t-lg"
-              src="https://t4.ftcdn.net/jpg/01/25/67/91/360_F_125679112_ECIW7IyeEpivJr3ESdjUX6roFizds3ww.jpg"
+              className=" rounded-t-lg"
+              src="https://rukminim2.flixcart.com/image/850/1000/kgmla4w0/pulse-oximeter/y/k/r/sahyog-wellness-led-type-fingertip-pulse-oximeter-big-digit-led-original-imafwtrb2dzhnujf.jpeg?q=20&crop=false"
               alt=""
             />
           </Link>
           <div className="p-5">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Radiology
+              WheelChair
             </h5>
           </div>
         </div>
-        <div className="max-w-sm m-2 bg-white hover:scale-105  rounded-lg shadow-md  bg-gradient-to-br from-green-100 to-blue-900">
+        <div className="bg-gradient-to-r mt-14 from-teal-300 via-green-100 to-white max-w-sm m-2 bg-white hover:scale-105  rounded-lg shadow-md">
           <Link onClick={loadcontent} to="/about">
             <img
               className="rounded-t-lg"
-              src="https://t4.ftcdn.net/jpg/02/89/11/17/360_F_289111791_j0ZsFCafoM4n47Pj6Nf1fVzC5OKXYtr7.jpg"
+              src="https://m.media-amazon.com/images/I/51agpgQn4AL.jpg"
               alt=""
             />
           </Link>
-          <div className="p-5">
+          <div className="p-5 ">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Pathology
             </h5>
           </div>
         </div>
-        <div className="max-w-sm m-2 bg-white hover:scale-105  rounded-lg shadow-md  bg-gradient-to-br from-green-100 to-blue-900">
+        <div className="bg-gradient-to-r mt-14 from-teal-300 via-green-100 to-white max-w-sm m-2 bg-white hover:scale-105  rounded-lg shadow-md ">
           <Link onClick={loadcontent} to="/about">
             <img
               className="rounded-t-lg"
-              src="https://freedesignfile.com/upload/2018/05/People-who-do-ECG-Stock-Photo.jpg"
+              src="https://aeoncare.in/cdn/shop/products/WalkingFramePremiumdynaatbestpriceinIndia.jpg?v=1642661974"
               alt=""
             />
           </Link>
@@ -56,11 +57,11 @@ export default function Tests(props) {
         id="test-container"
         className="container mt-5 p-3  rounded-t-3xl flex flex-wrap flex-row"
       >
-        <div className="max-w-sm m-2 bg-white hover:scale-105 rounded-lg shadow-md  bg-gradient-to-br from-green-100 to-blue-900">
+        <div className="bg-gradient-to-r mt-14 from-teal-300 via-green-100 to-white max-w-sm m-2 bg-white hover:scale-105  rounded-lg shadow-md">
           <Link onClick={loadcontent} to="/about">
             <img
               className="rounded-t-lg"
-              src="https://t4.ftcdn.net/jpg/01/25/67/91/360_F_125679112_ECIW7IyeEpivJr3ESdjUX6roFizds3ww.jpg"
+              src="https://m.media-amazon.com/images/I/51HFj13aktL._AC_UF1000,1000_QL80_.jpg"
               alt=""
             />
           </Link>
@@ -70,7 +71,7 @@ export default function Tests(props) {
             </h5>
           </div>
         </div>
-        <div className="max-w-sm m-2 bg-white hover:scale-105  rounded-lg shadow-md  bg-gradient-to-br from-green-100 to-blue-900">
+        <div className="bg-gradient-to-r mt-14 from-teal-300 via-green-100 to-white max-w-sm m-2 bg-white hover:scale-105  rounded-lg shadow-md">
           <Link onClick={loadcontent} to="/about">
             <img
               className="rounded-t-lg"
@@ -84,11 +85,11 @@ export default function Tests(props) {
             </h5>
           </div>
         </div>
-        <div className="max-w-sm m-2 bg-white hover:scale-105  rounded-lg shadow-md  bg-gradient-to-br from-green-100 to-blue-900">
+        <div className="bg-gradient-to-r mt-14 from-teal-300 via-green-100 to-white max-w-sm m-2 bg-white hover:scale-105  rounded-lg shadow-md">
           <Link onClick={loadcontent} to="/about">
             <img
               className="rounded-t-lg"
-              src="https://freedesignfile.com/upload/2018/05/People-who-do-ECG-Stock-Photo.jpg"
+              src="https://5.imimg.com/data5/GX/HI/MY-15598585/digital-bp-apparatus.jpg"
               alt=""
             />
           </Link>
