80841159b903ebc65f8c43d4d5a15edc63d04748
abtyagi15 <abtyagi150702@gmail.com>
2024-03-30 11:00:18 +0530

    verify otp controller route created

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { register,login } = require('../controllers/Auth');
+const { register,login,verifyOtp } = require('../controllers/Auth');
 
 router.post('/register',register);
 router.post('/login',login);
+router.post('/verify-email',verifyOtp);
 
 module.exports = router;
